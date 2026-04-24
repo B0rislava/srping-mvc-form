d33aa216480de94a3c2ab2e6690017a7c9d7774f
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2026-04-24 12:57:10 +0300

    Initial commit

---
+++ src/main/java/org/exercise/mvc/srpingmvcform/SrpingMvcFormApplication.java
@@ -0,0 +1,13 @@
+package org.exercise.mvc.srpingmvcform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingMvcFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingMvcFormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/exercise/mvc/srpingmvcform/SrpingMvcFormApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exercise.mvc.srpingmvcform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingMvcFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
